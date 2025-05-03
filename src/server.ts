7045750d40cb98fa273122edc24901631118c70c
abas14357 <abbas14357@gmail.com>
2025-05-03 17:03:21 +0500

    latest

--- src/server.ts
+++ src/server.ts
@@ -12,44 +12,44 @@ app.use(express.json()); // for POST support
 app.get('/api/issuecompliancecert', async (req, res) => {
   try {
   
-  //   Default Values for Sample  
-  //   const egsunit: EGSUnitInfo = {
-  //     uuid: "6f4d20e0-6bfe-4a80-9389-7dabe6620f12",
-  //     custom_id: "EGS1-886431145",
-  //     model: "IOS",
-  //     CRN_number: "454634645645654",
-  //     VAT_name: "Wesam Alzahir",
-  //     VAT_number: "399999999900003",
-  //     location: {
-  //         city: "Khobar",
-  //         city_subdivision: "West",
-  //         street: "King Fahahd st",
-  //         plot_identification: "0000",
-  //         building: "0000",
-  //         postal_zone: "31952"
-  //     },
-  //     branch_name: "My Branch Name",
-  //     branch_industry: "Food"
-  // };
-
+    // Default Values for Sample  
     const egsunit: EGSUnitInfo = {
-      uuid: uuidv4(),
-      custom_id: req.query.custom_id as string,
-      model: req.query.model as string,
-      CRN_number: req.query.crn as string,
-      VAT_name: req.query.vat_name as string,
-      VAT_number: req.query.vat_number as string,
+      uuid: "6f4d20e0-6bfe-4a80-9389-7dabe6620f12",
+      custom_id: "EGS1-886431145",
+      model: "IOS",
+      CRN_number: "454634645645654",
+      VAT_name: "Wesam Alzahir",
+      VAT_number: "399999999900003",
       location: {
-        city: req.query.city as string,
-        city_subdivision: req.query.subdivision as string,
-        street: req.query.street as string,
-        plot_identification: req.query.plot as string,
-        building: req.query.building as string,
-        postal_zone: req.query.postal as string,
+          city: "Khobar",
+          city_subdivision: "West",
+          street: "King Fahahd st",
+          plot_identification: "0000",
+          building: "0000",
+          postal_zone: "31952"
       },
-      branch_name: req.query.branch_name as string,
-      branch_industry: req.query.industry as string,
-    };
+      branch_name: "My Branch Name",
+      branch_industry: "Food"
+  };
+
+    // const egsunit: EGSUnitInfo = {
+    //   uuid: uuidv4(),
+    //   custom_id: req.query.custom_id as string,
+    //   model: req.query.model as string,
+    //   CRN_number: req.query.crn as string,
+    //   VAT_name: req.query.vat_name as string,
+    //   VAT_number: req.query.vat_number as string,
+    //   location: {
+    //     city: req.query.city as string,
+    //     city_subdivision: req.query.subdivision as string,
+    //     street: req.query.street as string,
+    //     plot_identification: req.query.plot as string,
+    //     building: req.query.building as string,
+    //     postal_zone: req.query.postal as string,
+    //   },
+    //   branch_name: req.query.branch_name as string,
+    //   branch_industry: req.query.industry as string,
+    // };
 
     const egs = new EGS(egsunit);
     await egs.generateNewKeysAndCSR(false, 'Multi-Techno');
