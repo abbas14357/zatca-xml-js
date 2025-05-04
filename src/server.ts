758e70eb5a7df39da50fc8f842fd16ec1a9f41c0
abas14357 <abbas14357@gmail.com>
2025-05-04 17:13:39 +0500

    latest

--- src/server.ts
+++ src/server.ts
@@ -32,6 +32,8 @@ app.get('/api/issuecompliancecert', async (req, res) => {
   //     branch_industry: "Food"
   // };
 
+  console.log('request parameters:', req.query);
+
   const uuid_p = uuidv4();
   const is_production_request = req.query.is_production ? req.query.is_production : false;
   const request_otp = req.query.request_otp ? req.query.request_otp : '123345'; 
