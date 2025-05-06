32c341938ec19290311282e4df379e0a32c4d20e
abas14357 <abbas14357@gmail.com>
2025-05-06 15:35:36 +0500

    latest

--- src/server.ts
+++ src/server.ts
@@ -549,7 +549,7 @@ app.post('/api/invoicecompliance', async (req, res) => {
       "MIICNjCCAdygAwIBAgIGAZagcaSJMAoGCCqGSM49BAMCMBUxEzARBgNVBAMMCmVJbnZvaWNpbmcwHhcNMjUwNTA1MTIzNTE5WhcNMzAwNTA0MjEwMDAwWjBWMRYwFAYDVQQDDA1FR1MxLTk2OTI4NDEwMRcwFQYDVQQLDA5NeSBCcmFuY2ggTmFtZTEWMBQGA1UECgwNV2VzYW0gQWx6YWhpcjELMAkGA1UEBhMCU0EwVjAQBgcqhkjOPQIBBgUrgQQACgNCAATpbLwXrITSvBnOePllbPD4uJkI41woQXzT5SXiYfgPt38T8vvEZpnTmTngb1A+bstPVheTtfYo0QSp1esSiKPHo4HZMIHWMAwGA1UdEwEB/wQCMAAwgcUGA1UdEQSBvTCBuqSBtzCBtDFNMEsGA1UEBAxEMS1NdWx0aS1UZWNobm98Mi1NVUxUSS1URUNITk98My1mMmU0M2U2My0wMjZlLTRhYTUtYWZiOC1iYmFhMjEwNGVkMjExHzAdBgoJkiaJk/IsZAEBDA8zOTk5OTk5OTk5MDAwMDMxDTALBgNVBAwMBDAxMDAxJDAiBgNVBBoMGzAwMDAgS2luZyBGYWhhaGQgc3QsIEtob2JhcjENMAsGA1UEDwwERm9vZDAKBggqhkjOPQQDAgNIADBFAiEAkIK5SwjSdrNYm4OtQwsMXvfN0BZREfv9zkE64MM9/i4CIEZ+7JO9FwE+cFSyjpCRNbTtInqmZzqFQ3dAD46k+Ta/\n" +
       "-----END CERTIFICATE-----";
 
-    egs.set({ compliance_certificate: compliance_certificate });
+    egs.set({ compliance_certificate: req.body.compliance_certificate });
 
     if (req.body.compliance_api_secret) {
       egs.set({ compliance_api_secret: req.body.compliance_api_secret });
