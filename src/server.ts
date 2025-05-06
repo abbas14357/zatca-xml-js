50fe43ea79181134c98337a748987bbab5ee86a8
abas14357 <abbas14357@gmail.com>
2025-05-06 12:14:01 +0500

    latest

--- src/server.ts
+++ src/server.ts
@@ -509,7 +509,7 @@ app.post('/api/signinvoice', async (req, res) => {
 
 app.post('/api/invoicecompliance', async (req, res) => {
   try {
-    console.log('invoice compliance body:', req.body.invoice_hash);
+    console.log('invoice compliance body:', req.body.compliance_certificate);
 
     const egsunit: EGSUnitInfo = {
       uuid: req.body.uuid as string,
